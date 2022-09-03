8f6ecd74f828b141bf54e569c1c74cceb87037f3
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2022-09-03 19:44:02 -0400

    Removed main()
    I was originally using this to shut off all the code to do testing
    things.. I no longer need this.

--- src/index.js
+++ src/index.js
@@ -1,48 +1,44 @@
-function main() {
-  const { Helpers } = require('./Helpers.js');
+const { Helpers } = require('./Helpers.js');
 
-  let serverName = 'SamCore';
+let serverName = 'SamCore';
 
-   /**
-   * SamCore will be doing all of the editing and manipulation
-   * of the json settings file.  So we can have it set up to
-   * autosave.  No need to refresh the file.
-   * 
-   * Also, if the file doesnt exist, lets create one
-   */
-  const editJsonFile = require('edit-json-file');
-  const runDirectory = process.cwd();
-  const SamCoreSettings = 'SamCoreSettings.json';
-  const filePath = `${runDirectory}/${SamCoreSettings}`
-  let file = editJsonFile(filePath, {autosave: true});
-  if (!Object.keys(file.get()).length) {
-    file.set(`packages.${serverName}`, Helpers.defaultPackage({
-      version: '1.0.0', // try and pull this from package file
-      installed: true,
-      persistent: true,
-      mandatory: true,
-      link: "https://github.com/vindennl48/Sam-SamCore"
-    }));
-  } 
-  
-  // Create server and run
-  const { Server } = require('./Server.js');
-  let SamCore = new Server(serverName);
-  SamCore
-    .addApiCall('doesNodeExist', function(packet, socket) {
-      packet.dataSent = packet.data;
-      packet.data     = false;
+  /**
+ * SamCore will be doing all of the editing and manipulation
+ * of the json settings file.  So we can have it set up to
+ * autosave.  No need to refresh the file.
+ * 
+ * Also, if the file doesnt exist, lets create one
+ */
+const editJsonFile = require('edit-json-file');
+const runDirectory = process.cwd();
+const SamCoreSettings = 'SamCoreSettings.json';
+const filePath = `${runDirectory}/${SamCoreSettings}`
+let file = editJsonFile(filePath, {autosave: true});
+if (!Object.keys(file.get()).length) {
+  file.set(`packages.${serverName}`, Helpers.defaultPackage({
+    version: '1.0.0', // try and pull this from package file
+    installed: true,
+    persistent: true,
+    mandatory: true,
+    link: "https://github.com/vindennl48/Sam-SamCore"
+  }));
+} 
 
-      if (packet.dataSent in this.sockets) {
-        packet.data = true;
-      }
-      this.return(packet);
-    })
-    .addApiCall('helloWorld', function(packet, socket) {
-      packet.data = 'helloWorld! ' + packet.data;
-      this.return(packet);
-    })
-    .run();
-}
+// Create server and run
+const { Server } = require('./Server.js');
+let SamCore = new Server(serverName);
+SamCore
+  .addApiCall('doesNodeExist', function(packet, socket) {
+    packet.dataSent = packet.data;
+    packet.data     = false;
 
-main();
+    if (packet.dataSent in this.sockets) {
+      packet.data = true;
+    }
+    this.return(packet);
+  })
+  .addApiCall('helloWorld', function(packet, socket) {
+    packet.data = 'helloWorld! ' + packet.data;
+    this.return(packet);
+  })
+  .run();
