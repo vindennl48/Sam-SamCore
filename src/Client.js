32ae717384846252f20259b7503d6d00f18a6ec3
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2022-09-24 17:07:20 -0400

    Added async/await support
    Now we can use async and await to provide a much cleaner experience and
    reduce the amount of callbacks that were originally used.

--- src/Client.js
+++ src/Client.js
@@ -3,15 +3,14 @@ const { Helpers }   = require('./Helpers.js');
 
 class Client {
   /**
-   * 
-   * @param {string} nodeName 
-   *  Name of this node
-   * @param {string} serverName 
-   *  Name of the server to connect to
-   * @param {bool} isSilent 
-   *  Do you want node-ipc to be verbose? (default=true)
-   * @returns this
-   */
+    * @param {string} nodeName 
+    *  Name of this node
+    * @param {string} serverName 
+    *  Name of the server to connect to
+    * @param {bool} isSilent 
+    *  Do you want node-ipc to be verbose? (default=true)
+    * @returns this
+    */
   constructor(nodeName, serverName, isSilent=true) {
     this.nodeName          = nodeName;
     this.serverName        = serverName;
@@ -28,81 +27,62 @@ class Client {
   }
 
   /**
-   * Return data back from an API Call 
-   * 
-   * @param {json} packet 
-   *  packet received for API call
-   */
-  return(packet) {
-    this.ipc.of[this.serverName].emit(
-      `${this.serverName}.return`,
-      packet
-    );
-  }
+    * Call an API call from another node
+    *
+    * @param {string} receiver
+    *   The node in which you are calling the api call from
+    * @param {string} apiCall
+    *   The name of the api call to call
+    * @param {all} data
+    *   Can be any data type. This is the data required by the api call
+    */
+  async callApi(receiver, apiCall, data={}) {
+    let promise = await new Promise(function(resolve, reject) {
+      /**
+        * The returnCode is to force a unique callback for each api call.  This
+        * allows us to prevent mixing receiving data up with other calls being
+        * made at the same time.
+        */
+      let returnCode = Date.now();
 
-  returnError(packet, errorMessage="Default Error Message") {
-    packet.errorMessage = errorMessage;
-
-    this.ipc.of[this.serverName].emit(
-      `${this.serverName}.returnError`,
-      packet
-    );
-  }
-
-  /**
-   * Used to call an API from another node
-   * 
-   * @param {string} receiver 
-   *  Name of node to call
-   * @param {string} apiCall 
-   *  Name of API to call
-   * @param {json} data 
-   *  Any data required to send for the API call. If no data
-   *  is required, send an empty object = {}
-   */
-  callApi(receiver, apiCall, data={}, callback=null) {
-    let returnCode = null;
-
-    // create one-time callback
-    if (callback != null) {
-      returnCode = Date.now();
-
+      // Creates the ipc listener for the return data
       this.ipc.of[this.serverName].on(
-        `${this.nodeName}.${receiver}.${apiCall}.${returnCode}`,
-        callback.bind(this),
-        true // run once
+        `${receiver}.${apiCall}.return.${this.nodeName}.${returnCode}`,
+        resolve,
+        true // setting this true will remove this listener once used
       );
-    }
 
-    let packet = {
-      sender:     this.nodeName,
-      receiver:   receiver,
-      apiCall:    apiCall,
-      returnCode: returnCode,
-      data:       data
-    };
-    
-    /**
-     * If we calling an api from another node and not
-     * from SamCore
-     */
-    this.ipc.of[this.serverName].emit(
-      receiver == this.serverName ? `${this.serverName}.${apiCall}` :
-        `${this.serverName}.send`,
-      packet
-    );
+      // Generates the packet needed to send thru the server
+      let packet = {
+        sender:     this.nodeName,
+        receiver:   receiver,
+        apiCall:    apiCall,
+        returnCode: returnCode,
+        data:       data
+      };
+
+      // Sends out the api call
+      this.ipc.of[this.serverName].emit(
+        receiver == this.serverName ? `${this.serverName}.${apiCall}` :
+          `${this.serverName}.send`,
+        packet
+      );
+
+    }.bind(this));
+
+    return promise;
   }
 
   /**
-   * Used for adding an API call to this node
-   * 
-   * @param {string} call 
-   *  Name of API call
-   * @param {function(data)} callBack 
-   *  Function to run when API is called. This must include
-   *  'packet' as an argument for the callback function.
-   * @returns this
-   */
+    * Used for adding an API call to this node
+    * 
+    * @param {string} call 
+    *  Name of API call
+    * @param {function(data)} callBack 
+    *  Function to run when API is called. This must include
+    *  'packet' as an argument for the callback function.
+    * @returns this
+    */
   addApiCall(call, callBack) {
     this.calls.push(`${this.nodeName}.${call}`);
     this.callBacks.push(callBack.bind(this));
@@ -110,18 +90,18 @@ class Client {
   }
 
   /**
-   * Used for adding a hook to another node's API call
-   * 
-   * @param {string} call 
-   *  For hooking into an API request:
-   *    senderNodeName.apiCall
-   *  For hooking into an API response:
-   *    senderNodeName.apiNodeName.apiCall
-   * @param {function(data)} callBack 
-   *  Function to run when API is called. This must include
-   *  'packet' as an argument for the callback function.
-   * @returns this
-   */
+    * Used for adding a hook to another node's API call
+    * 
+    * @param {string} call 
+    *  For hooking into an API request:
+    *    senderNodeName.apiCall
+    *  For hooking into an API response:
+    *    senderNodeName.apiNodeName.apiCall
+    * @param {function(data)} callBack 
+    *  Function to run when API is called. This must include
+    *  'packet' as an argument for the callback function.
+    * @returns this
+    */
   addHook(call, callBack) {
     this.calls.push(call);
     this.callBacks.push(callBack.bind(this));
@@ -129,183 +109,146 @@ class Client {
   }
 
   /**
-   * Used for adding return API calls
-   * 
-   * @param {string} apiNodeName 
-   *  Name of node that we are receiving an answer from
-   * @param {string} call 
-   *  Name of API call
-   * @param {function(data)} callBack 
-   *  Function to run when data is received. Must have
-   *  'packet' as an argument for the callback function.
-   * @returns this
-   */
-  addReturnCall(apiNodeName, call, callBack) {
-    this.calls.push(`${this.nodeName}.${apiNodeName}.${call}`);
-    this.callBacks.push(callBack.bind(this));
-    return this;
+    * Return data back from an API Call 
+    * 
+    * @param {json} packet 
+    *  packet received for API call
+    */
+  return(packet) {
+    this.ipc.of[this.serverName].emit(`${this.serverName}.return`, packet);
   }
 
   /**
-   * Used to start up the IPC connection.  This function MUST
-   * be called. 
-   * 
-   * @param {function} onConnect 
-   *  Optional main function call for this Node.  Called after
-   *  connection to server is established.
-   */
+    * Return an error from API call
+    *
+    * @param {json} packet
+    *   packet returning from API Call
+    * @param {string} errorMessage
+    *   Message being sent back to caller of api call
+    */
+  returnError(packet, errorMessage='Default Error Message') {
+    packet.errorMessage = errorMessage;
+    this.ipc.of[this.serverName].emit(`${this.serverName}.returnError`, packet);
+  }
+
+  /**
+    * Used to start up the IPC connection.  This function MUST
+    * be called. 
+    * 
+    * @param {function} onConnect 
+    *  Optional main function call for this Node.  Called after
+    *  connection to server is established.
+    */
   // run(onConnect=null) {
-  run(onInit=null, onConnect=null) {
-    this.ipc.connectTo(this.serverName, function() {
-      /**
-      * This establishes the connection between this node and the Server,
-      * SamCore. Documentation can be found in the node-ipc github repo.
+  async run(onConnect=null) {
+    /**
+      * Make sure we establish connection first
       */
-      this.ipc.of[this.serverName].on('connect', function() {
-        /**
-        * When the node initially connects to the server, this will
-        * send it's node name to the server so the server can collect
-        * the socket connection for future use.
-        */
-        this.callApi(this.serverName, 'nodeInit', this.nodeName);
+    await this._connect();
 
-        /**
-        * If we use onInit argument, we need this function inside of the onInit
-        * callback.  If there is no onInit argument, we can just run it as-is.
-        */
-        function loadCallbacks() {
-          /**
-          * All of this node's API calls
-          */
-          for (let i=0; i<this.calls.length; i++) {
-            this.ipc.of[this.serverName].on(this.calls[i], this.callBacks[i]);
-          }
-
-          /**
-          * Here is the main loop function for this node.  It uses the
-          * onConnect callback if it exists.
-          */
-          if (onConnect != null) { (onConnect.bind(this))(); }
-        }
-
-        /**
-        * Here is the logic for the above function based off of the existence
-        * of the onInit argument. 
-        *
-        * Examples are below.
-        */
-        if (onInit != null) {
-          (onInit.bind(this))(function(){ loadCallbacks(); }.bind(this));
-        } else {
-          loadCallbacks();
-        }
-
-      }.bind(this));
-
-      /**
+    /**
       * If connection ends with the server, shutdown program.  The server
       * is responsible for restarting all nodes in the network.
       */
-      this.ipc.of[this.serverName].on('disconnect', function() {
-        this.ipc.disconnect(this.serverName);
-      }.bind(this));
+    this.ipc.of[this.serverName].on('disconnect', function() {
+      this.ipc.disconnect(this.serverName);
+    }.bind(this));
 
-      /**
+    /**
       * Built-in for sending messages to other nodes.  Easy way
       * to be able to debug connection issues.
       */
-      this.ipc.of[this.serverName].on(`${this.nodeName}.message`, function(packet) {
-        Helpers.log({leader: 'arrow', loud: true}, `Message from "${packet.sender}":`, packet.data);
-      }.bind(this));
-
+    this.ipc.of[this.serverName].on(`${this.nodeName}.message`, function(packet) {
+      Helpers.log({leader: 'arrow', loud: true}, `Message from '${packet.sender}':`, packet.data);
     }.bind(this));
+
+    /**
+      * Make sure server gives go-ahead before loading custom api calls
+      */
+    await this._networkOperational();
+
+    /**
+      * Now we can load all of this node's API calls
+      */
+    for (let i=0; i<this.calls.length; i++) {
+      this.ipc.of[this.serverName].on(this.calls[i], this.callBacks[i]);
+    }
+
+    /**
+      * Here is the main loop function for this node.  It uses the
+      * onConnect callback if it exists.
+      */
+    if (onConnect != null) { (onConnect.bind(this))(); }
   }
+
+  async _connect() {
+    let promise = await new Promise(function(resolve, reject) {
+      /**
+        * This establishes the connection between this node and the Server,
+        * SamCore. Documentation can be found in the node-ipc github repo.
+        */
+      this.ipc.connectTo(this.serverName, async function() {
+        // When connection is established, run nodeInit
+        this.ipc.of[this.serverName].on('connect', async function() {
+          /**
+            * When the node initially connects to the server, this will
+            * send it's node name to the server so the server can collect
+            * the socket connection for future use.
+            */
+          await this.callApi(this.serverName, 'nodeInit', this.nodeName);
+          resolve();
+        }.bind(this));
+      }.bind(this));
+    }.bind(this));
+
+    return promise;
+  }
+
+  async _networkOperational() {
+    let answer = false;
+
+    while (!answer) {
+      let result = await this.callApi(this.serverName, 'greenLight');
+      if (result.data === true) {
+        answer = true;
+        // Helpers.log({leader: 'highlight', loud: false}, 'GO!');
+      } else {
+        // Helpers.log({leader: 'highlight', loud: false}, 'Waiting for green light..');
+        await this._timeout(1000);
+      }
+    }
+
+    return answer;
+  }
+
+  async _timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 }
 
+
 /**
- * Example Code
- */
+  * Example Code
+  */
 function main() {
   let nodeName   = 'testclient';
   let serverName = 'testserver';
-  let myNode     = new Client(nodeName, serverName);
-  let mySettings = {};    // we will initialize this in onInit below
-  let username   = null;  // we will initialize this in onInit below
+  let myNode     = new Client(nodeName, serverName/*, false*/);
 
-  // API
   myNode
-    .addApiCall('helloWorld', function(packet) {
-      packet.data = 'HELLO WORLD! ' + packet.data;
+    .addApiCall('helloMe', function(packet) {
+      packet.data = packet.data + ' ME!';
       this.return(packet);
     })
 
-  /**
-  * Hooks:
-  *   Need to add the exact IPC message that you want to
-  *   hook on to:
-  *     for sent calls:     receiverNodeName.apiCall
-  *     for receiver calls: senderNodeName.receiverNodeName.apiCall
-  */
-  myNode
-    .addHook('testserver.butter', function(packet) {
-      console.log('Hook before: ', packet.data);
-    })
-    .addHook('testclient.testserver.butter', function(packet) {
-      console.log('Hook after: ', packet.data);
-    })
+    .run(async function() {
+      let packet = await this.callApi(nodeName, 'helloMe', 'All of this with');
 
-  /**
-  * Return data from api calls. Only used for specific situations. Most of the
-  * time, we will be using callbacks from 'this.apiCall()' functions.
-  *
-  * Return calls need to be in this format:
-  *   thisNodeName.receivedByNodeName.apiCall
-  * This helps filter out where the call came from.
-  */
-  myNode
-    .addReturnCall('testclient', 'helloWorld', function(packet) {
-      console.log('Result: ', packet.data);
-    })
-    .addReturnCall('testserver', 'butter', function(packet) {
-      console.log('Result: ', packet.data);
-    })
-
-  // Main function
-  myNode
-    .run(onConnect=function() {
-      // Main Function. Called after connection is established
-      this.callApi('testserver', 'butter', 'Butter the bread...');
-    });
-
-  // onInit feature
-  myNode
-    /**
-    * When using the 'onInit' feature, there is one important thing that needs
-    * to be done.  We need a callback as an argument and we need to call this
-    * callback inside the onInit function.  This will call the onConnect
-    * function as well as initialize all api calls defined above.
-    *
-    * This allows us to initialize the node before allowing any other nodes to
-    * call any api commands.
-    */
-    .run(onInit=function(callback){
-      /**
-      * Put any initialization code here, ie. lets get our settings from
-      * samcore.
-      */
-      this.callApi('testserver', 'getSettings', nodeName, function(packet) {
-        mySettngs = packet.data; // << save our settings locally
-
-        this.callApi('testserver', 'getUsername', {}, function(packet) {
-          username = packet.data; // << save our username locally
-          callback();  // << We are finally calling the callback here
-        });
-      });
-    }, onConnect=function(){
-      // \/\/ we are now calling our api in the main loop
-      this.callApi('testserver', 'butter', 'Butter the bread...');
+      Helpers.log({leader: 'arrow', loud: true}, 'packet: ', packet.data);
     });
 }
-// main();  // uncomment to use example code
+
+main(); // uncomment to use example code
 
 module.exports = { Client };
--- src/Server.js
+++ src/Server.js
@@ -2,17 +2,15 @@ const { IPCModule } = require('node-ipc');
 const { Helpers }   = require('./Helpers.js');
 
 class Server {
-
   /**
-   * 
-   * @param {string} serverName 
-   *  Name you would like the server to be.
-   *  Note: this will be the name you must call from
-   *        all of your nodes in the network.
-   * @param {bool} isSilent 
-   *  Allow or dis-allow verbose mode for node-ipc.
-   * @returns this
-   */
+    * @param {string} serverName 
+    *  Name you would like the server to be.
+    *  Note: this will be the name you must call from
+    *        all of your nodes in the network.
+    * @param {bool} isSilent 
+    *  Allow or dis-allow verbose mode for node-ipc.
+    * @returns this
+    */
   constructor(serverName, isSilent=true) {
     this.serverName        = serverName;
     this.ipc               = new IPCModule;
@@ -24,45 +22,45 @@ class Server {
     this.callBacks = [];
     this.sockets   = [];
 
+    // when set to true, all nodes can start communicating
+    this.greenLight = false;
+
     return this;
   }
 
   /**
-   * Used to send data from a node api call to the receiving
-   * node that made the call. The Server in this network is the
-   * middleman for all communication.
-   * 
-   * @param {json} packet 
-   *  packet received from node
-   */
+    * Used to send data from a node api call to the receiving
+    * node that made the call. The Server in this network is the
+    * middleman for all communication.
+    * 
+    * @param {json} packet 
+    *  packet received from node
+    */
   return(packet) {
-    let returnCall = `${packet.sender}.${packet.receiver}.${packet.apiCall}`;
+    // receiver.apiCall.return.sender.returnCode
+    let returnCall = `${packet.receiver}.${packet.apiCall}.return.${packet.sender}`;
     if (packet.returnCode != null) {
-      returnCall = returnCall + `.${packet.returnCode}`;
-    }
-
-    if (packet.apiCall == 'onError') {
-      returnCall = 'onError';
+      returnCall += `.${packet.returnCode}`;
     }
 
     this.ipc.server.broadcast(returnCall, packet);
   }
 
-  returnError(packet, errorMessage=null) {
-    if (errorMessage != null) {
-      packet.errorMessage = errorMessage;
+  returnError(packet) {
+    if ( !('errorMessage' in packet) ) {
+      packet.errorMessage = 'Default error message';
     }
 
     this.ipc.server.broadcast(`${packet.sender}.onError`, packet);
   }
 
   /**
-   * Used to send the api call to the destined node. The Server in this
-   * network is the middleman for all communication.
-   * 
-   * @param {json} packet 
-   *  packet received from node
-   */
+    * Used to send the api call to the destined node. The Server in this
+    * network is the middleman for all communication.
+    * 
+    * @param {json} packet 
+    *  packet received from node
+    */
   send(packet) {
     this.ipc.server.broadcast(
       `${packet.receiver}.${packet.apiCall}`,
@@ -71,81 +69,88 @@ class Server {
   }
 
   /**
-   * Add an api call for other nodes to call
-   * 
-   * @param {string} call 
-   *  Name of API call
-   * @param {function(packet, socket)} callBack 
-   *  Function to run when this API call is called.  Must include arguments.
-   * @returns this
-   */
+    * Add an api call for other nodes to call
+    * 
+    * @param {string} call 
+    *  Name of API call
+    * @param {function(packet, socket)} callBack 
+    *  Function to run when this API call is called.  Must include arguments.
+    * @returns this
+    */
   addApiCall(call, callBack) {
     this.calls.push(`${this.serverName}.${call}`);
     this.callBacks.push(callBack.bind(this));
     return this;
   }
 
+  _timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
   /**
-   * Main function to start the server up
-   */
-  run() {
+    * Main function to start the server up
+    */
+  run(onConnect=null) {
     this.ipc.serve(function() {
       this.ipc.server.on('*', function(message, packet, socket) {
         if (message == 'connect') { return; }
-        // Helpers.log({leader: 'highlight'}, `Message: ${message}, Packet: `, packet, `, Socket: ${socket.connecting}`);
-        if (message.endsWith('return')) {
+
+        if (message == `${this.serverName}.return`) {
           this.return(packet);
         }
-        else if (message.endsWith('returnError')) {
+        else if (message == `${this.serverName}.returnError`) {
           this.returnError(packet);
         }
-        else {
+        else if (message == `${this.serverName}.send`) {
           this.send(packet);
         }
       }.bind(this));
 
       /**
-       * This allows the server to collect sockets of the
-       * connected nodes in the network.
-       */
+        * This allows the server to collect sockets of the
+        * connected nodes in the network.
+        */
       this.ipc.server.on(`${this.serverName}.nodeInit`, function(packet, socket) {
         this.sockets[packet.data] = socket;
+
+        // This should be bare minimum return on all API calls
+        packet.bdata = packet.data;  // Make copy of original data
+        packet.data  = true;         // Add our return data
+        this.return(packet);         // Send the packet back to sender
       }.bind(this));
 
       /**
-       * A send function for when Node A calls an api from
-       * Node B.
-       */
-// Have to get rid of these because of the hook broadcast.
-// This was double sending calls
-//      this.ipc.server.on(`${this.serverName}.send`, function(packet, socket) {
-//        if ('receiver' in packet) { this.send(packet); }
-//      }.bind(this));
-//
-//      /**
-//       * A return function for when Node A calls an api from
-//       * Node B and Node B sends a response.
-//       */
-//      this.ipc.server.on(`${this.serverName}.return`, function(packet, socket) {
-//        if ('sender' in packet) { this.return(packet); }
-//      }.bind(this));
+        * This tells nodes in the network if it is ok to start broadcasting
+        * after initializing.
+        */
+      this.ipc.server.on(`${this.serverName}.greenLight`, function(packet, socket) {
+        // This should be bare minimum return on all API calls
+        packet.bdata = packet.data;     // Make copy of original data
+        packet.data  = this.greenLight; // Add our return data
+        this.return(packet);            // Send the packet back to sender
+      }.bind(this));
 
       /**
-       * Built-in for sending messages to other nodes.  Easy way
-       * to be able to debug connection issues.
-       */
+        * Built-in for sending messages to other nodes.  Easy way
+        * to be able to debug connection issues.
+        */
       this.ipc.server.on(`${this.serverName}.message`, function(packet, socket) {
         Helpers.log(
           {leader: 'arrow', loud: true},
-          `Message from "${packet.sender}":`,
+          `Message from '${packet.sender}':`,
           packet.data
         );
+
+        // This should be bare minimum return on all API calls
+        packet.bdata = packet.data;  // Make copy of original data
+        packet.data  = true;         // Add our return data
+        this.return(packet);         // Send the packet back to sender
       }.bind(this));
 
       /**
-       * Used to make sure we remove any sockets that no longer exists.
-       * When a socket disconnects, we find the socket and delete it.
-       */
+        * Used to make sure we remove any sockets that no longer exists.
+        * When a socket disconnects, we find the socket and delete it.
+        */
       this.ipc.server.on('socket.disconnected', function(socket, destroyedSocketID) {
         let keys = Object.keys(this.sockets);
         for (var key in keys) {
@@ -155,14 +160,23 @@ class Server {
             delete this.sockets[key];
           }
         }
+
+        // no return required here
       }.bind(this));
 
       /**
-       * Add in all custom API calls
-       */
+        * Add in all custom API calls
+        */
       for (let i=0; i<this.calls.length; i++) {
         this.ipc.server.on(this.calls[i], this.callBacks[i]);
       }
+
+
+      /**
+        * Here is the main loop function for this server.  It uses the
+        * onConnect callback if it exists.
+        */
+      if (onConnect != null) { (onConnect.bind(this))(); }
     }.bind(this));
 
     this.ipc.server.start();
@@ -171,18 +185,34 @@ class Server {
 
 
 /**
- * Example Code
- */
+  * Example Code
+  */
 function main() {
   let myServer = new Server('testserver');
 
   myServer
-    .addApiCall('butter', function(packet, socket) {
-      packet.data = packet.data + 'with butter!';
+    .addApiCall('helloWorld', function(packet) {
+      packet.bdata = packet.data;
+      packet.data  = 'Hello World! ' + packet.data ;
       this.return(packet);
     })
-    .run();
+
+    .run(async function() {
+
+      // Use to boot up all other nodes
+      // const exec = require('child_process').exec;
+      // exec('node src/Client.js',
+      //   function (error, stdout, stderr) {
+      //     Helpers.log({leader: 'warning', loud: false}, 'Client Died');
+      //   }
+      // );
+
+      // placeholder to start up the other nodes
+      // await this._timeout(10000);
+      this.greenLight = true; // This is required to allow other nodes to start
+    });
 }
-// main();  // uncomment to use example code
+
+main();  // uncomment to use example code
 
 module.exports = { Server };
